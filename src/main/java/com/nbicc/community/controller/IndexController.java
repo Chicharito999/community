6481f739a2f9cc175a9396b9c07088faa38a5009
Mingfu Zhao <1109646702@qq.com>
2020-02-09 17:54:41 +0800

    add publish

--- src/main/java/com/nbicc/community/controller/IndexController.java
+++ src/main/java/com/nbicc/community/controller/IndexController.java
@@ -16,14 +16,17 @@ public class IndexController {
     @GetMapping({"/","/index","index.html"})
     public String index(HttpServletRequest request){
         Cookie[] cookies = request.getCookies();
-        for(Cookie cookie: cookies){
-            if ("token".equals(cookie.getName())){
-                String token = cookie.getValue();
-                User user = userMapper.findByToken(token);
-                request.getSession().setAttribute("user",user);
-                break;
+        if(cookies!=null && cookies.length>0){
+            for(Cookie cookie: cookies){
+                if ("token".equals(cookie.getName())){
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    request.getSession().setAttribute("user",user);
+                    break;
+                }
             }
         }
+
 
         return "index";
     }
